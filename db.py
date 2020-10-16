6c55539badc4df2c32c5c30e9dfeff830bb03e32
aahan63 <sharmaaahan15@gmail.com>
2020-10-16 10:52:43 +0530

    add connect in db

--- db.py
+++ db.py
@@ -1,17 +1,31 @@
+from loguru import logger
+import psycopg2
+
+
 class Database:
     """PostgreSQL Database class."""
 
-    def __init__(
-            self,
-            DATABASE_HOST,
-            DATABASE_USERNAME,
-            DATABASE_PASSWORD,
-            DATABASE_PORT,
-            DATABASE_NAME
-        ):
-        self.host = DATABASE_HOST
-        self.username = DATABASE_USERNAME
-        self.password = DATABASE_PASSWORD
-        self.port = DATABASE_PORT
-        self.dbname = DATABASE_NAME
+    def __init__(self, config):
+        self.host = config.DATABASE_HOST
+        self.username = config.DATABASE_USERNAME
+        self.password = config.DATABASE_PASSWORD
+        self.port = config.DATABASE_PORT
+        self.dbname = config.DATABASE_NAME
         self.conn = None
+
+    def connect(self):
+        """Connect to a Postgres database."""
+        if self.conn is None:
+            try:
+                self.conn = psycopg2.connect(
+                    host=self.host,
+                    user=self.username,
+                    password=self.password,
+                    port=self.port,
+                    dbname=self.dbname
+                )
+            except psycopg2.DatabaseError as e:
+                LOGGER.error(e)
+                raise e
+            finally:
+                LOGGER.info('Connection opened successfully.')
