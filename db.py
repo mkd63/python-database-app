8afe237ed506f23ab9fad9fa07930e0edcfa3a93
aahan63 <sharmaaahan15@gmail.com>
2020-10-16 10:49:34 +0530

    added db.py

---
+++ db.py
@@ -0,0 +1,17 @@
+class Database:
+    """PostgreSQL Database class."""
+
+    def __init__(
+            self,
+            DATABASE_HOST,
+            DATABASE_USERNAME,
+            DATABASE_PASSWORD,
+            DATABASE_PORT,
+            DATABASE_NAME
+        ):
+        self.host = DATABASE_HOST
+        self.username = DATABASE_USERNAME
+        self.password = DATABASE_PASSWORD
+        self.port = DATABASE_PORT
+        self.dbname = DATABASE_NAME
+        self.conn = None
