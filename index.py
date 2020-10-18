0eb21c58ac4cdd31486f94669f5ec5198822cecb
aahan63 <sharmaaahan15@gmail.com>
2020-10-19 03:14:48 +0530

    created app index structure

--- index.py
+++ index.py
@@ -1,17 +1,36 @@
-from os import environ, path
-from dotenv import load_dotenv
+from db import Database
+from loguru import logger
+import config
 
-# Load variables from .env
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+def reg():
+    new_user_email = input("Enter Your Email: ")
+    new_user_name = input("Enter Your username: ")
+    new_user_password = input("Enter Your Password: ")
+    db_instance = Database(config)
+    db_instance.connect()
+    #db.select_rows("""Select * from users""")
+    rows = db_instance.run_query("""SELECT * from users""")
+    new_user_id = len(rows) + 1
+    db_instance.insert(new_user_id,new_user_name,new_user_password,new_user_email)
 
+def login():
+    print("login")
 
-# Database config
-DATABASE_HOST = environ.get('DATABASE_HOST')
-DATABASE_USERNAME = environ.get('DATABASE_USERNAME')
-DATABASE_PASSWORD = environ.get('DATABASE_PASSWORD')
-DATABASE_PORT = environ.get('DATABASE_PORT')
-DATABASE_NAME = environ.get('DATABASE_NAME')
-
-# SQL queries
-SQL_QUERIES_FOLDER = 'sql'
+print(
+"""
+    Welcome user !
+    Please authorize.
+"""
+)
+new_user = input("Do you already have an account ? (y/n)")
+entryVal_flag = False
+while(entryVal_flag != True):
+    if(new_user == "y"):
+        entryVal_flag = True
+        login()
+    elif(new_user == "n"):
+        entryVal_flag = True
+        reg()
+    else:
+        print("Invalid entry. Please try again.")
+        new_user = input("Do you already have an account ? (y/n)")
